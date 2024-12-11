1ac7dbe41cc0be5534c86a31eccc088e7c5a20a6
CharlesL-Sec <173039445+CharlesL-Sec@users.noreply.github.com>
2024-12-11 11:47:50 +0000

    Update cloneGroup.js

--- cloneGroup.js
+++ cloneGroup.js
@@ -1,9 +1,9 @@
 # Create a new UI page to capture the user information
 
-create UI page
-- goto
--- system UI -> UI Pages (sys_ui_page.list)
+### To Create the UI page (From). This page will appear in the ServiceNow menu bar.
+- goto system UI -> UI Pages (sys_ui_page.list)
 - Click new
+- Provide the name for this UI page e.g. __CLone user groups__
 
 
 
